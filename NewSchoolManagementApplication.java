f3a813e7369d85a241516ab2162740da5ce1ebc2
Shraddha <126331132+Shraddhat286@users.noreply.github.com>
2024-05-24 21:15:59 +0530

    Add files via upload

---
+++ NewSchoolManagementApplication.java
@@ -0,0 +1,13 @@
+package com.shd.newSchoolManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSchoolManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewSchoolManagementApplication.class, args);
+	}
+
+}
